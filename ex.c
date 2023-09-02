2b2bd5599ed7627609e6a2f9f1b5efc66f11e91b
sungmin <sungminmail0@naver.com>
2023-09-02 11:04:56 +0900

    초기 커밋.

---
+++ ex.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#define buffer_size 10
+
+int read_lin(char str[], int limit);
+
+int main() {
+	char buffer[buffer_size];
+	int k;
+	while (1)
+	{
+		printf("$ ");
+		k = read_line(buffer, buffer_size);
+		printf("%s:%d\n",buffer,k);
+	}
+	return 0;
+}
+	
+int read_line(char str[], int limit)
+{
+	int ch, i = 0;
+	while((ch = getchar()) != '\n')
+	{	
+	
+			str[i++] = ch;
+	
+	}
+
+	str[i] = '\0';
+	return i;
+}
